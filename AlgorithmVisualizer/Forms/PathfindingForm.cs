15e85aba373e84dff55d0919f392851bc50c538a
sandaminim03 <sandaminim03@gmail.com>
2026-05-09 13:46:51 +0530

    Update PathfindingForm.cs

--- AlgorithmVisualizer/Forms/PathfindingForm.cs
+++ AlgorithmVisualizer/Forms/PathfindingForm.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,6 +10,8 @@ namespace AlgorithmVisualizer
 {
     public partial class Pathfinding_Visualizer : Form
     {
+        int gridSize = 20;
+        int animationSpeed = 100;
         public Pathfinding_Visualizer()
         {
             InitializeComponent();
@@ -17,7 +19,17 @@ namespace AlgorithmVisualizer
 
         private void button1_Click(object sender, EventArgs e)
         {
-            new SettingsForm().Show();
+            SettingsForm form = new SettingsForm(gridSize, animationSpeed);
+
+            if (form.ShowDialog() == DialogResult.OK)
+            {
+                gridSize = form.SelectedSize;
+                animationSpeed = form.SelectedSpeed;
+
+                // Apply changes later
+                // Example:
+                // Recreate grid with new size
+            }
         }
 
         private void btnback_Click(object sender, EventArgs e)
