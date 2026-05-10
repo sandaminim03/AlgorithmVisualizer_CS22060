8f6a6bb5762fbb17f11976d218ab9ea9c2584278
sandaminim03 <sandaminim03@gmail.com>
2026-05-10 19:36:19 +0530

    Add files via upload

---
+++ AlgorithmVisualizer/Algorithms/InsertionSort.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmVisualizer.Algorithms
+{
+    public class InsertionSort
+    {
+        private int[] data;
+        private int i;
+        private int j;
+        private bool isSorting;
+
+        public void Initialize(int[] array)
+        {
+            this.data = array;
+            this.i = 1;
+            this.j = 0;
+            this.isSorting = true;
+        }
+
+        public bool Step(Action<int, int, bool> updateUI)
+        {
+            if (!isSorting || i >= data.Length)
+            {
+                isSorting = false;
+                return true; // Sorting is finished
+            }
+
+            if (j >= 0 && data[j] > data[j + 1])
+            {
+                // Perform the swap
+                int temp = data[j];
+                data[j] = data[j + 1];
+                data[j + 1] = temp;
+
+                updateUI(j, j + 1, true);
+                j--;
+                return false; // Step completed, continue sorting
+            }
+            else
+            {
+                i++;
+                j = i - 1;
+                return false;
+            }
+        }
+    }
+
+}
---
+++ AlgorithmVisualizer/Algorithms/QuickSort.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmVisualizer.Algorithms
+{
+    public class QuickSort
+    {
+        private int[] data;
+        private Stack<(int low, int high)> stack;
+        private int i, j, pivot, low, high;
+        private bool partitioning;
+        private bool isSorting;
+
+        public void Initialize(int[] array, int start, int end)
+        {
+            this.data = array;
+            this.stack = new Stack<(int, int)>();
+            this.stack.Push((start, end));
+            this.isSorting = true;
+            this.partitioning = false;
+        }
+
+        public bool Step(Action<int, int, bool> updateUI)
+        {
+            if (!isSorting) return true;
+
+            if (!partitioning)
+            {
+                if (stack.Count == 0) return true;
+
+                var range = stack.Pop();
+                low = range.low;
+                high = range.high;
+
+                if (low >= high) return false;
+
+                pivot = data[high];
+                i = low - 1;
+                j = low;
+                partitioning = true;
+            }
+
+            if (partitioning)
+            {
+                if (j <= high - 1)
+                {
+                    if (data[j] < pivot)
+                    {
+                        i++;
+                        Swap(i, j);
+                        updateUI(i, j, true);
+                    }
+                    else
+                    {
+                        updateUI(i, j, false);
+                    }
+                    j++;
+                }
+                else
+                {
+                    Swap(i + 1, high);
+                    updateUI(i + 1, high, true);
+
+                    int pIdx = i + 1;
+                    stack.Push((pIdx + 1, high));
+                    stack.Push((low, pIdx - 1));
+                    partitioning = false;
+                }
+            }
+            return false;
+        }
+
+        private void Swap(int a, int b)
+        {
+            int temp = data[a];
+            data[a] = data[b];
+            data[b] = temp;
+        }
+    }
+
+}
